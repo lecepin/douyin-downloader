966d88ea642e23c33d9860cd9fb3112128ed0dd8
乐平 <383810086@qq.com>
2022-08-05 21:47:36 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "antd/dist/antd.min.css";
-
 import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
