dffcd77db29bc21eca8024e7f8dd807131ea9d90
乐平 <383810086@qq.com>
2022-08-05 21:49:02 +0800

    Update config-overrides.js

--- config-overrides.js
+++ config-overrides.js
@@ -1,16 +1,9 @@
 const { override, addLessLoader, adjustStyleLoaders } = require('customize-cra');
 
 module.exports = override(
-  addLessLoader({
-    lessOptions: {
-      javascriptEnabled: true,
-    },
-  }),
+  addLessLoader({ lessOptions: { javascriptEnabled: true } }),
   adjustStyleLoaders(({ use: [, , postcss] }) => {
-    const postcssOptions = postcss.options;
-    postcss.options = { postcssOptions };
+    postcss.options = { postcss.options };
   }),
-  function (config) {   
-    return config;
-  },
+  function (config) { return config; },
 );
